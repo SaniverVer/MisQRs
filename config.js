7737033d7ce5baf6033603c884c914d0671ad895
SaniverVer <logistica@saniver.com.mx>
2024-08-26 11:22:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/a/macros/saniver.com.mx/s/AKfycbxjMeNHgxxgnwDrwZcmtSGc93RlzQrtfhALIT34dFg2k4AVV8MP64LepDsQdT73SBGE/exec";
